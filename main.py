863475ea3f28b7c22d2581256ed8bff22d3f54f0
dalor <dalor@i.ua>
2019-03-12 01:20:04 +0200

    First

---
+++ main.py
@@ -0,0 +1,76 @@
+import discord
+import os
+import youtube_dl
+from discord.ext import commands
+from music import MusicController
+import re
+
+TOKEN = os.environ['BOT_ID']
+
+client = commands.Bot(command_prefix='')
+controller = MusicController()
+
+async def connect_voice_channel(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    if channel:
+        server = ctx.message.server
+        voice_client = client.voice_client_in(server)
+        if not voice_client:
+            voice_client = await client.join_voice_channel(channel)
+        elif channel != voice_client.channel:
+            await voice_client.move_to(channel)
+        return voice_client
+    else:
+        await client.send_message(ctx.message.channel, 'Зайди в голосовой канал')
+
+@client.command(pass_context=True)
+async def join(ctx):
+    await connect_voice_channel(ctx)
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    if voice_client:
+        await voice_client.disconnect()
+        text = 'Ладно'
+    else:
+        text = 'И что?'
+    await client.send_message(ctx.message.channel, text)
+
+@client.command(pass_context=True)
+async def find(ctx, url):
+    voice_client = await connect_voice_channel(ctx)
+    if voice_client:
+        controller.find(ctx.message.server.id, voice_client, url)
+
+@client.command(pass_context=True)
+async def play(ctx):
+    controller.play(ctx.message.server.id)
+
+@client.command(pass_context=True)
+async def pause(ctx):
+    controller.pause(ctx.message.server.id)
+
+@client.command(pass_context=True)
+async def skip(ctx):
+    controller.skip(ctx.message.server.id)
+
+@client.command(pass_context=True)
+async def volume(ctx, vol):
+    vol = re.match('([0-9]{1,3})', vol)
+    if vol:
+        vol = float(vol[1]) / 100
+        if vol >= 0. and vol <= 2.:
+            controller.volume(ctx.message.server.id, vol)
+
+@client.command(pass_context=True)
+async def stop(ctx):
+    controller.stop(ctx.message.server.id) 
+
+@client.command(pass_context=True)
+async def all(ctx):
+    titles = controller.get_titles(ctx.message.server.id)
+    await client.send_message(ctx.message.channel, '\n'.join(titles))
+
+client.run(TOKEN)
---
+++ music.py
@@ -0,0 +1,167 @@
+import youtube_dl
+from threading import Thread
+from time import sleep
+import os
+from uuid import uuid4 as random_token
+
+class DLParser(Thread):
+    def __init__(self, url, queue, voice_client):
+        Thread.__init__(self)
+        self.url = url
+        self.queue = queue
+        self.voice_client = voice_client
+        self.ydl_opts = {
+            'format': 'bestaudio/best',
+            'get-url': True,
+            'get-title': True,
+            'get-thumbnail': True,
+            'skip-download': True,
+            'get-duration': True,
+            #'quiet': True
+        }
+
+    def check_by_youtube_dl(self, url):
+        try:
+            with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
+                return ydl.extract_info(url, download=False)
+        except:
+            return None
+
+    def run(self):
+        res = self.check_by_youtube_dl(self.url)
+        if res:
+            if 'entries' in res:
+                for item in res['entries']:
+                   self.queue.add(QueueItem(item, self.voice_client))
+            else:
+                self.queue.add(QueueItem(res, self.voice_client))
+    
+
+class QueueItem:
+    def __init__(self, info, voice_client):
+        self.url = info['url']
+        self.title = info['title']
+        self.voice_client = voice_client
+        self.player = None
+
+    def exit(self):
+        self.player.stop()
+
+    def set_player(self, vol):
+        self.player = self.voice_client.create_ffmpeg_player(self.url)
+        self.player.volume = vol
+
+    def __repr__(self):
+        return self.title
+
+class QueueController(Thread):
+    def __init__(self, server_id, volume=1.):
+        Thread.__init__(self)
+        self.queue = []
+        self.wait = 3
+        self.server_id = server_id
+        self.volume = volume
+   
+    def add(self, item):
+        self.queue.append(item)
+
+    def find_by_url(self, url, voice_client):
+        DLParser(url, self, voice_client).start()
+    
+    def go_next(self, anyway):
+        if self.queue and (self.queue[0].player or anyway):
+            self.queue[0].exit()
+            self.queue.pop(0)
+        if self.queue:
+            return self.queue[0]
+        else:
+            None
+
+    def play_next(self, anyway=True):
+        next_ = self.go_next(anyway)
+        if next_:
+            next_.set_player(self.volume)
+            next_.player.start()
+       
+    def run(self):
+        while True:
+            if self.queue:
+                if not self.queue[0].player or self.queue[0].player.is_done():
+                    self.play_next(anyway=False)
+            sleep(self.wait)
+
+    def the_last(old):
+        def new(self, *args):
+            if self.queue:
+                return old(self, self.queue[0], *args)
+        return new
+
+    def the_last(old):
+        def new(self, *args):
+            if self.queue:
+                return old(self, self.queue[0], *args)
+        return new
+    
+    @the_last
+    def play(self, last):
+        last.player.resume()
+
+    @the_last
+    def pause(self, last):
+        last.player.pause()
+
+    @the_last
+    def set_volume(self, last, vol):
+        self.volume = vol
+        last.player.volume = vol
+
+    @the_last
+    def stop(self, last):
+        self.queue.clear()
+        last.player.stop()
+
+class MusicController:
+    def __init__(self, db=None):
+        self.db = db
+        self.channels = {}
+    
+    def get_channel(self, server_id):
+        channel = self.channels.get(server_id)
+        if not channel:
+            channel = QueueController(server_id)
+            self.channels[server_id] = channel
+            channel.start()
+        return channel
+
+    def put_channel(old):
+        def new(self, server_id, *args):
+            return old(self, self.get_channel(server_id), *args)
+        return new
+
+    @put_channel
+    def find(self, channel, voice_client, url):
+        channel.find_by_url(url, voice_client)
+
+    @put_channel
+    def play(self, channel):
+        channel.play()
+
+    @put_channel
+    def pause(self, channel):
+        channel.pause()
+
+    @put_channel
+    def skip(self, channel):
+        channel.play_next()
+
+    @put_channel
+    def volume(self, channel, vol):
+        channel.set_volume(vol)
+
+    @put_channel
+    def stop(self, channel):
+        channel.stop()
+
+    @put_channel
+    def get_titles(self, channel):
+        return [one.title for one in channel.queue]
